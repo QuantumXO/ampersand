5bbe1230a6de3af1300c2741341c0b0c5d0b97a6
Sikorskiy Dmitriy <sikorskiy.dima@yandex.ru>
2018-02-11 13:51:30 +0200

    ampersand

---
+++ index.php
@@ -0,0 +1,164 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+	<title>Document</title>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta name="Keywords" content="">
+	<meta name="Description" content="">
+	<meta name="author" content="">
+	<link rel="icon" href="" type="image/x-icon">
+	<link rel="stylesheet" href="css/reset.css">
+	<link rel="stylesheet" href="css/style.css">
+ 	<!--[if lt IE 9]><script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script><![endif]-->
+</head>
+<body>
+	<header>
+		<div class="row">
+			<div class="header_menu-top">
+				<a href="#">About me</a><a href="#">Contact</a>
+			</div>
+			<div class="header_logo">
+				<a href="#"><span> & </span>Ampersand</a>
+			</div>        
+		</div>
+			<div class="header_bottom">
+				<div class="row">
+					<form>
+						<span>Search</span>
+						<input type="text"/>
+						<input type="submit" value="" />
+					</form>
+					<ul role="navigation">
+						<li><a href="#">Articles</a></li>
+						<li><a href="#">News</a></li>
+						<li><a href="#">Random Thoughts</a></li>
+						<li><a href="#">Other things</a></li>
+					</ul>
+				</div>
+			</div>
+	</header>
+	<div class="conteiner row clearfix">
+		<div class="content">
+			<article>
+				<p class="article_title">cras dictum pretium tortor id scelerisque</p>
+				<div class="article_about"><span class="uppercase">august</span> 12, 2011 by <span class="uppercase">john doe</span><span class="article_about-comments">13 comments</span></div>
+				<div class="article_post">
+					<img src="img/image.jpg" alt="post image" class="article_img"/>
+					<p class="article_text">
+						Donec orci risus, porta ac commodo ut, gravida eget libero. Vestibulum tincidunt risus at nulla vulputate volutpat. Aliquam lobortis lectus sit amet magna interdum et adipiscing justo convallis. Phasellus vulputate nunc neque. Maecenas rhoncus magna vel massa fringilla vitae viverra nibh bibendum. Nullam egestas pulvinar elit, at adipiscing dui condimentum ac. Suspendisse potenti.
+						<a href="#" class="read_more">read more  →</a>
+					</p>
+				</div>
+			</article>		
+			<article>
+				<p class="article_title">cras dictum pretium tortor id scelerisque</p>
+				<div class="article_about"><span class="uppercase">august</span> 12, 2011 by <span class="uppercase">john doe</span><span class="article_about-comments">13 comments</span></div>
+				<div class="article_post">
+					<img src="img/image.jpg" alt="post image" class="article_img"/>
+					<p class="article_text">
+						Donec orci risus, porta ac commodo ut, gravida eget libero. Vestibulum tincidunt risus at nulla vulputate volutpat. Aliquam lobortis lectus sit amet magna interdum et adipiscing justo convallis. Phasellus vulputate nunc neque. Maecenas rhoncus magna vel massa fringilla vitae viverra nibh bibendum. Nullam egestas pulvinar elit, at adipiscing dui condimentum ac. Suspendisse potenti.
+						<a href="#" class="read_more">read more  →</a>
+					</p>
+				</div>
+			</article>	
+			<ul class="pagination">
+				<li><a href="http://localhost/">1</a></li>
+				<li><a href="#">2</a></li>
+				<li><a href="#">3</a></li>
+				<li><a href="#">4</a></li>
+			</ul>			
+		</div>
+		<aside>
+			<div class="block">
+				<div class="block_name">about this blog</div>
+				<div class="block_post">Nam augue eros, imperdiet at dignissim sed, adipiscing eu leo. Vestibulum mi orci, auctor a iaculis sed, tempus suscipit felis. Integer rutrum tristique ipsum, ac dignissim justo commodo non.</div>
+			</div>
+			<div class="block">
+				<div class="block_name">latest posts</div>
+				<div class="block_post">
+					<img src="img/jpg-mini.jpg" alt="" class="article_img block_post_img"/>
+					<div class="block_post_title">integer mattis ornare</div>
+					<p class="block_post_text">Donec ut urna odio. Proin molestie mauris sit amet felis auctor ut ullamcorper dolor aliquet...</p>
+				</div>
+				<div class="block_post">
+					<img src="img/jpg-mini.jpg" alt="" class="article_img block_post_img"/>
+					<div class="block_post_title">integer mattis ornare</div>
+					<p class="block_post_text">Donec ut urna odio. Proin molestie mauris sit amet felis auctor ut ullamcorper dolor aliquet...</p>
+				</div>
+				<div class="block_post">
+					<img src="img/jpg-mini.jpg" alt="" class="article_img block_post_img"/>
+					<div class="block_post_title">integer mattis ornare</div>
+					<p class="block_post_text">Donec ut urna odio. Proin molestie mauris sit amet felis auctor ut ullamcorper dolor aliquet...</p>
+				</div>
+			</div>
+			<div class="block">
+				<div class="block_name">categories</div>
+				<div class="block_post">
+					<table>
+						<tr>
+							<td><a href="#">Fusce semper</a></td>
+							<td><a href="#">Fusce semper</a></td>
+						</tr>
+						<tr>
+							<td><a href="#">Mollis dui</a></td>
+							<td><a href="#">Mollis dui</a></td>
+						</tr>
+						<tr>
+							<td><a href="#">Vel posuere</a></td>
+							<td><a href="#">Vel posuere</a></td>
+						</tr>
+						<tr>
+							<td><a href="#">Nullam accumsan</a></td>
+							<td><a href="#">Nullam accumsan</a></td>
+						</tr>
+						<tr>
+							<td><a href="#">Commodo arcu</a></td>
+							<td><a href="#">Commodo arcu</a></td>
+						</tr>
+						<tr>
+							<td><a href="#">Fringilla risus</a></td>
+							<td><a href="#">Fringilla risus</a></td>
+						</tr>
+						<tr>
+							<td><a href="#">Semper et</a></td>
+							<td><a href="#">Semper et</a></td>
+						</tr>
+					</table>
+				</div>
+			</div>
+		</aside>
+	</div>
+	<footer>
+		<div class="row">
+			<div class="block">
+				<div class="block_name">about this theme</div>
+				<div class="block_post"><a href="#">Nam augue</a> eros, imperdiet at dignissim sed, adipiscing eu leo. <a href="#">Vestibulum mi orci</a>, auctor a iaculis sed, tempus suscipit felis. Integer rutrum tristique ipsum, ac dignissim justo.
+commodo non. Morbi vestibulum lectus eget orci mollis porttitor. Phasellus porttitor luctus dignissim.</div>
+			</div>
+			<div class="block">
+				<div class="block_name">about this theme</div>
+				<div class="block_post"><a href="#">Nam augue</a> eros, imperdiet at dignissim sed, adipiscing eu leo. <a href="#">Vestibulum mi orci</a>, auctor a iaculis sed, tempus suscipit felis. Integer rutrum tristique ipsum, ac dignissim justo.
+commodo non. Morbi vestibulum lectus eget orci mollis porttitor. Phasellus porttitor luctus dignissim.</div>
+			</div>
+			<div class="block">
+				<div class="block_name">about this theme</div>
+				<div class="block_post"><a href="#">Nam augue</a> eros, imperdiet at dignissim sed, adipiscing eu leo. <a href="#">Vestibulum mi orci</a>, auctor a iaculis sed, tempus suscipit felis. Integer rutrum tristique ipsum, ac dignissim justo.
+commodo non. Morbi vestibulum lectus eget orci mollis porttitor. Phasellus porttitor luctus dignissim.</div>
+			</div>			
+		</div>
+	</footer>
+<script src="js/jquery-1.11.3.min.js"></script>
+<script>
+	$(function () {                                      // Когда страница загрузится
+    $('.pagination li a').each(function () {             // получаем все нужные нам ссылки
+        var location = window.location.href; // получаем адрес страницы
+        var link = this.href;                // получаем адрес ссылки
+        if(location == link) {               // при совпадении адреса ссылки и адреса окна
+            $(this).addClass('pagination-active');  //добавляем класс
+        }
+    });
+});
+</script>	
+</body>
+</html>
